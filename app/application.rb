26524fd7b43831838c5c4176a7121c3f028271e8
github username <github email address>
2018-07-16 14:13:26 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,21 +1,49 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
+      if req.path.match(/items/)
+        @@items.each do |item|
+          resp.write "#{item}\n"
+        end
+      elsif req.path.match(/search/)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+      elsif req.path.match(/cart/)
+          if @@cart.length == 0
+            resp.write "Your cart is empty"
+          else
+            @@cart.each do |item|
+              resp.write "#{item}\n"
+            end
+          end
+      elsif req.path.match(/add/)
+
+        food_item = req.params["item"]
+
+        if @@items.include?(food_item)
+          @@cart << food_item
+          resp.write "added #{food_item}\n"
+        else
+          resp.write "We don't have that item"
+        end
+      else
+        resp.write "Path Not Found"
     end
+
+    # @@items.each do |item|
+    #   @@cart << item
+    #   resp.write "added #{item}\n"
+    # end
+
+    # if @@items.length == 0
+    #   resp.write "We don't have that item"
 
     resp.finish
   end
